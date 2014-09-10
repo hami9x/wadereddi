340bc56e4b5ca4f6395f882cad30e88de9ee04a8
phaikawl <phaikawl@gmail.com>
2014-09-10 10:37:33 +0700

    sorting by new and top

--- client/clientside.go
+++ client/clientside.go
@@ -2,15 +2,22 @@ package client
 
 import (
 	"math/rand"
+	"sort"
 	"time"
 
 	"github.com/phaikawl/wade"
-	"github.com/phaikawl/wade/elements/menu"
+	"github.com/phaikawl/wade/custom"
+	"github.com/phaikawl/wade/taglibs/menu"
 )
 
 func init() {
 	rand.Seed(time.Now().Unix())
 }
+
+const (
+	RankModeTop    = "top"
+	RankModeLatest = "latest"
+)
 
 type (
 	Comment struct {
@@ -38,37 +45,41 @@ type (
 	}
 
 	VoteBoxModel struct {
-		wade.BaseProto
-		Vote *Score
+		custom.BaseProto
+		Vote     *Score
+		ReSortFn func()
 	}
 
-	HomeModel struct {
-		Posts []*Post
+	HomeScope struct {
+		*wade.BaseScope
+		Posts    []*Post
+		SortMode string
 	}
 )
 
-func (s *Score) VoteUp() {
-	if s.Voted != 1 {
-		s.Score++
-		s.Voted = 1
-	} else {
-		s.Score--
-		s.Voted = 0
-	}
-}
-
-func (s *Score) VoteDown() {
-	if s.Voted != -1 {
-		s.Score--
-		s.Voted = -1
-	} else {
-		s.Score++
-		s.Voted = 0
-	}
+func (p *Post) Score() int {
+	return p.Vote.Score
 }
 
 func NewScore(score int) *Score {
 	return &Score{score, 0}
+}
+
+func (s *Score) VoteUp() {
+	s.Score += 1 - s.Voted
+	s.Voted = 1
+}
+
+func (s *Score) VoteDown() {
+	s.Score += -1 - s.Voted
+	s.Voted = -1
+}
+
+func (m *VoteBoxModel) WrapReSort(voteFn func()) func() {
+	return func() {
+		voteFn()
+		m.ReSortFn()
+	}
 }
 
 var (
@@ -141,22 +152,63 @@ func GenComments(n int) (ret []*Comment) {
 	return
 }
 
+func (home *HomeScope) SortByTop() {
+	home.SortMode = RankModeTop
+	sort.Sort(PostsByTop(home.Posts))
+	home.ApplyChanges(&home.Posts)
+}
+
+func (home *HomeScope) SortByLatest() {
+	home.SortMode = RankModeLatest
+	sort.Sort(PostsByLatest(home.Posts))
+	home.ApplyChanges(&home.Posts)
+}
+
+func (home *HomeScope) TopRefresh() {
+	if home.SortMode == RankModeTop {
+		home.SortByTop()
+	}
+}
+
 func InitFunc(r wade.Registration) {
 	r.RegisterDisplayScopes([]wade.PageDesc{
 		wade.MakePage("pg-home", "/home", "Home"),
 		wade.MakePage("pg-comments", "/comments/:postid", "Comments for %v"),
 	}, nil)
 
-	r.RegisterCustomTags(menu.CustomTags()...)
-	r.RegisterCustomTags(wade.CustomTag{
+	r.RegisterCustomTags(menu.HtmlTags()...)
+	r.RegisterCustomTags(custom.HtmlTag{
 		Name:       "votebox",
 		Attributes: []string{},
 		Prototype:  &VoteBoxModel{},
 	})
 
-	r.RegisterController("pg-home", func(p *wade.PageCtrl) interface{} {
-		return &HomeModel{
-			Posts: Database,
+	r.RegisterController("pg-home", func(p *wade.BaseScope) wade.ScopeModel {
+		posts := Database
+
+		m := &HomeScope{
+			BaseScope: p,
+			Posts:     posts,
+			SortMode:  RankModeTop,
 		}
+
+		m.SortByTop()
+
+		return m
 	})
 }
+
+// Sorting boilerplates, in real world apps when a database is used, these things
+// wouldn't be necessary
+type (
+	PostsByTop    []*Post
+	PostsByLatest []*Post
+)
+
+func (a PostsByTop) Len() int           { return len(a) }
+func (a PostsByTop) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a PostsByTop) Less(i, j int) bool { return a[i].Score() > a[j].Score() }
+
+func (a PostsByLatest) Len() int           { return len(a) }
+func (a PostsByLatest) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a PostsByLatest) Less(i, j int) bool { return a[i].Time < a[j].Time }
