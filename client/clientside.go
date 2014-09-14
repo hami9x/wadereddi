47802cae5aa53651c95c59adf56e70c4bfad2445
phaikawl <phaikawl@gmail.com>
2014-09-14 13:32:05 +0700

    comment page

--- client/clientside.go
+++ client/clientside.go
@@ -14,12 +14,70 @@ func init() {
 	rand.Seed(time.Now().Unix())
 }
 
+type RankModeInfo struct {
+	Name          string
+	DisplayedName string
+}
+
 const (
 	RankModeTop    = "top"
 	RankModeLatest = "latest"
 )
 
+var (
+	RankModes = []RankModeInfo{
+		RankModeInfo{RankModeTop, "Top"},
+		RankModeInfo{RankModeLatest, "Latest"},
+	}
+)
+
+var (
+	CurrentUser = "Somebody"
+
+	PostsSort = map[string]func(posts []*Post){
+		RankModeTop: func(posts []*Post) {
+			sort.Sort(PostsByTop(posts))
+		},
+		RankModeLatest: func(posts []*Post) {
+			sort.Sort(PostsByLatest(posts))
+		},
+	}
+
+	CommentsSort = map[string]func(comments []*Comment){
+		RankModeTop: func(comments []*Comment) {
+			sort.Sort(CommentsByTop(comments))
+		},
+		RankModeLatest: func(comments []*Comment) {
+			sort.Sort(CommentsByLatest(comments))
+		},
+	}
+)
+
 type (
+	RankedList interface {
+		Sort(rankMode string)
+		ApplyChanges()
+	}
+
+	ListRank struct {
+		RankMode string
+		List     RankedList
+	}
+
+	ChangeApplier interface {
+		ApplyChanges(interface{})
+	}
+
+	PostsRank struct {
+		List []*Post
+		p    ChangeApplier
+	}
+
+	CommentsRank struct {
+		List []*Comment
+		p    ChangeApplier
+	}
+
 	Comment struct {
 		Author  string
 		Content string
@@ -52,8 +110,16 @@ type (
 
 	HomeScope struct {
 		*wade.BaseScope
-		Posts    []*Post
-		SortMode string
+		Rank      *ListRank
+		Posts     []*Post
+		RankModes []RankModeInfo
+	}
+
+	CommentsScope struct {
+		*wade.BaseScope
+		Rank      *ListRank
+		Post      *Post
+		RankModes []RankModeInfo
 	}
 )
 
@@ -61,29 +127,51 @@ func (p *Post) Score() int {
 	return p.Vote.Score
 }
 
+func (c *Comment) Score() int {
+	return c.Vote.Score
+}
+
 func NewScore(score int) *Score {
 	return &Score{score, 0}
 }
 
 func (s *Score) VoteUp() {
-	s.Score += 1 - s.Voted
-	s.Voted = 1
+	switch s.Voted {
+	case -1:
+		s.Score += 2
+		s.Voted = 1
+	case 0:
+		s.Score++
+		s.Voted = 1
+	case 1:
+		s.Score--
+		s.Voted = 0
+	}
 }
 
 func (s *Score) VoteDown() {
-	s.Score += -1 - s.Voted
-	s.Voted = -1
+	switch s.Voted {
+	case 1:
+		s.Score -= 2
+		s.Voted = -1
+	case 0:
+		s.Score--
+		s.Voted = -1
+	case -1:
+		s.Score++
+		s.Voted = 0
+	}
 }
 
-func (m *VoteBoxModel) WrapReSort(voteFn func()) func() {
-	return func() {
-		voteFn()
+func (m *VoteBoxModel) ReSort(voteFn func()) {
+	voteFn()
+	if m.ReSortFn != nil {
 		m.ReSortFn()
 	}
 }
 
 var (
-	RandomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	RandomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789            "
 	Database    = []*Post{
 		&Post{
 			Id:       3,
@@ -142,8 +230,8 @@ func GenComments(n int) (ret []*Comment) {
 	ret = make([]*Comment, n)
 	for i, _ := range ret {
 		ret[i] = &Comment{
-			Author:  NewStrLen(5),
-			Content: NewStrLen(30),
+			Author:  NewStrLen(3 + rand.Intn(5)),
+			Content: NewStrLen(rand.Intn(5 + 50)),
 			Time:    rand.Intn(100),
 			Vote:    NewScore(rand.Intn(2)),
 		}
@@ -152,34 +240,81 @@ func GenComments(n int) (ret []*Comment) {
 	return
 }
 
-func (home *HomeScope) SortByTop() {
-	home.SortMode = RankModeTop
-	sort.Sort(PostsByTop(home.Posts))
-	home.ApplyChanges(&home.Posts)
+func (lr *ListRank) SortBy(mode string) {
+	lr.RankMode = mode
+	lr.List.Sort(mode)
+	lr.List.ApplyChanges()
 }
 
-func (home *HomeScope) SortByLatest() {
-	home.SortMode = RankModeLatest
-	sort.Sort(PostsByLatest(home.Posts))
-	home.ApplyChanges(&home.Posts)
-}
-
-func (home *HomeScope) TopRefresh() {
-	if home.SortMode == RankModeTop {
-		home.SortByTop()
+func (lr *ListRank) TopRefresh() {
+	if lr.RankMode == RankModeTop {
+		lr.List.Sort(RankModeTop)
 	}
+}
+
+func (r PostsRank) Sort(rankMode string) {
+	PostsSort[rankMode](r.List)
+}
+
+func (r PostsRank) ApplyChanges() {
+	r.p.ApplyChanges(&r.List)
+}
+
+func (r CommentsRank) Sort(rankMode string) {
+	CommentsSort[rankMode](r.List)
+}
+
+func (r CommentsRank) ApplyChanges() {
+	r.p.ApplyChanges(&r.List)
+}
+
+func postById(id int) (p *Post, ok bool) {
+	for _, post := range Database {
+		if post.Id == id {
+			return post, true
+		}
+	}
+
+	return
+}
+
+func getLink(scope *wade.BaseScope, post *Post) string {
+	if post.Link != "" {
+		return post.Link
+	}
+
+	url, err := scope.Url("pg-comments", post.Id)
+	if err != nil {
+		panic(err)
+	}
+
+	return url
+}
+
+func (s *HomeScope) GetLink(post *Post) string {
+	return getLink(s.BaseScope, post)
+}
+
+func (s *CommentsScope) GetLink(post *Post) string {
+	return getLink(s.BaseScope, post)
 }
 
 func InitFunc(r wade.Registration) {
 	r.RegisterDisplayScopes([]wade.PageDesc{
 		wade.MakePage("pg-home", "/home", "Home"),
 		wade.MakePage("pg-comments", "/comments/:postid", "Comments for %v"),
-	}, nil)
+		wade.MakePage("pg-404", "/notfound", "404 Page Not Found"),
+	}, []wade.PageGroupDesc{
+		wade.MakePageGroup("grp-main", []string{"pg-home", "pg-comments"}),
+	})
+
+	r.RegisterNotFoundPage("pg-404")
 
 	r.RegisterCustomTags(menu.HtmlTags()...)
+
 	r.RegisterCustomTags(custom.HtmlTag{
 		Name:       "votebox",
-		Attributes: []string{},
+		Attributes: []string{"Vote", "ReSortFn"},
 		Prototype:  &VoteBoxModel{},
 	})
 
@@ -189,10 +324,43 @@ func InitFunc(r wade.Registration) {
 		m := &HomeScope{
 			BaseScope: p,
 			Posts:     posts,
-			SortMode:  RankModeTop,
+			Rank: &ListRank{
+				RankMode: RankModeTop,
+				List:     PostsRank{posts, p},
+			},
+			RankModes: RankModes,
 		}
 
-		m.SortByTop()
+		m.Rank.SortBy(m.Rank.RankMode)
+
+		return m
+	})
+
+	r.RegisterController("pg-comments", func(p *wade.BaseScope) wade.ScopeModel {
+		var postId int
+		err := p.GetParam("postid", &postId)
+		if err != nil {
+			p.RedirectToPage("pg-404")
+		}
+
+		post, ok := postById(postId)
+		if !ok {
+			p.RedirectToPage("pg-404")
+		}
+
+		p.FormatTitle(post.Title)
+
+		m := &CommentsScope{
+			BaseScope: p,
+			Post:      post,
+			Rank: &ListRank{
+				RankMode: RankModeTop,
+				List:     CommentsRank{post.Comments, p},
+			},
+			RankModes: RankModes,
+		}
+
+		m.Rank.SortBy(m.Rank.RankMode)
 
 		return m
 	})
@@ -201,8 +369,10 @@ func InitFunc(r wade.Registration) {
 // Sorting boilerplates, in real world apps when a database is used, these things
 // wouldn't be necessary
 type (
-	PostsByTop    []*Post
-	PostsByLatest []*Post
+	PostsByTop       []*Post
+	PostsByLatest    []*Post
+	CommentsByTop    []*Comment
+	CommentsByLatest []*Comment
 )
 
 func (a PostsByTop) Len() int           { return len(a) }
@@ -212,3 +382,11 @@ func (a PostsByTop) Less(i, j int) bool { return a[i].Score() > a[j].Score() }
 func (a PostsByLatest) Len() int           { return len(a) }
 func (a PostsByLatest) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a PostsByLatest) Less(i, j int) bool { return a[i].Time < a[j].Time }
+
+func (a CommentsByTop) Len() int           { return len(a) }
+func (a CommentsByTop) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a CommentsByTop) Less(i, j int) bool { return a[i].Score() > a[j].Score() }
+
+func (a CommentsByLatest) Len() int           { return len(a) }
+func (a CommentsByLatest) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a CommentsByLatest) Less(i, j int) bool { return a[i].Time < a[j].Time }
