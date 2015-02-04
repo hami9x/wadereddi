e0759f25663f64bd653f94bbc24dc83ecac679d0
phaikawl <phaikawl@gmail.com>
2015-02-04 20:00:27 +0700

    small bug fix on comments page

--- clientmain.go
+++ clientmain.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/phaikawl/wade/rt"
 	"github.com/phaikawl/wade/platform/clientside"
+	"github.com/phaikawl/wade/rt"
 	"github.com/phaikawl/wadereddi/client"
 )
 
 func main() {
-	ap := rt.NewApp(app.Config{
+	app := rt.NewApp(rt.Config{
 		BasePath: "/web",
 	}, clientside.CreateBackend())
 
-	err := ap.Start(client.App{ap})
+	err := app.Start(client.App{app})
 	if err != nil {
 		panic(fmt.Sprintf("Failed to load, error: %v.", err.Error()))
 	}
